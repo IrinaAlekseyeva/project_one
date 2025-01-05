be2c409b43ce7664e9ad68b2ef723ed444af0ce0
Irina Alekseyeva <irinaalekseyeva@MacBook-Pro.local>
2025-01-04 23:29:31 -0800

    fixed mistake in Spots model

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -27,9 +27,6 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       foreignKey: true
     },
-      primaryKey: true
-    },
-    ownerId: DataTypes.INTEGER,
     address: DataTypes.STRING,
     city: DataTypes.STRING,
     state: DataTypes.STRING,
