d93b029bc5c96695a81ab168ac862f81e8d71ba8
Irina Alekseyeva <irinaalekseyeva@MacBook-Pro.local>
2025-01-04 23:54:43 -0800

    1

--- backend/db/migrations/20241210050041-create-user.js
+++ backend/db/migrations/20241210050041-create-user.js
@@ -50,7 +50,7 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    options.tableName = "Users";
+    options.tableName = "User";
     return queryInterface.dropTable(options);
   }
 };
