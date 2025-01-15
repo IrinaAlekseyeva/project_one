fe4ccb1ee1c017f5860c9315b3628c1b01a155b4
Irina Alekseyeva <irinaalekseyeva@MacBook-Pro.local>
2025-01-14 19:20:11 -0800

    2

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,16 +1,6 @@
 const { sequelize } = require('./db/models');
 
-try {
-  sequelize.authenticate()
-    .then(() => {
-      console.log('Connection to the database established successfully.');
-    })
-    .catch((err) => {
-      console.error('Unable to connect to the database:', err);
-    });
-} catch (err) {
-  console.error('Error during authentication:', err);
-}
+
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if (!data.includes(process.env.SCHEMA)) {
     await sequelize.createSchema(process.env.SCHEMA); // schema is a particular part of database that will be used for this project
